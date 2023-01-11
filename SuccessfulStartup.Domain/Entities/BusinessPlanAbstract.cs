4bf584ba04cebddd50a3a1a888940419aa6a3ba6
Tim Honchel <tim.honchel@gmail.com>
2023-01-11 11:48:18 -0500

    added domain entity abstract class for business plans

---
+++ SuccessfulStartup.Domain/Entities/BusinessPlanAbstract.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace SuccessfulStartup.Domain.Entities
+{
+    internal abstract class BusinessPlanAbstract
+    {
+        protected abstract int Id { get; set; }
+        protected abstract string Name { get; set; }
+        protected abstract string Description { get; set; }
+
+    }
+}
