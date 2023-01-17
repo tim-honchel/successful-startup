c79995534b08ad415ebcc6e063434a7d7df0cdf1
Tim Honchel <tim.honchel@gmail.com>
2023-01-17 12:39:27 -0500

    added AuthorizeView to pages, used separate service configuration class for data layer

---
+++ SuccessfulStartup.Data/DataLayerConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components.Authorization; // for AuthenticationStateProvider
+using Microsoft.AspNetCore.Identity.UI.Services; //for IEmailSender
+using Microsoft.EntityFrameworkCore; // for UseSqlServer
+using Microsoft.Extensions.Configuration; // for ConfigurationBuilder
+using Microsoft.Extensions.DependencyInjection; // for IServiceCollection
+using SuccessfulStartup.Data.APIs;
+using SuccessfulStartup.Data.Authentication;
+using SuccessfulStartup.Data.Contexts;
+using SuccessfulStartup.Data.Repositories.WriteOnly;
+using SuccessfulStartup.Domain.Repositories.WriteOnly;
+
+namespace SuccessfulStartup.Data
+{
+    public static class DataLayerConfiguration
+    {
+        private const string connectionKey = "ConnectionStrings:IdentityConnectionString";
+        private const string fileLocation = "appsettings.json";
+        private static string connectionString = new ConfigurationBuilder().AddJsonFile(fileLocation).Build()[connectionKey]; // gets database connection string from appsettings.json
+
+        public static IServiceCollection AddDataScope(this IServiceCollection services)
+        {
+            services.AddDbContextFactory<AuthenticationDbContext>(options => options.UseSqlServer(connectionString));
+            services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<AuthenticationDbContext>(); // adds default UI for Identity, eliminating need to create custom register and login pages, also requires account verification prior to first login
+            services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<AppUser>>(); // periodically checks whether user credentials are still valid
+            services.AddScoped<IWriteOnlyApi, WriteOnlyApi>();
+            services.AddScoped<IReadOnlyApi, ReadOnlyApi>();
+            services.AddScoped<MappingProfile>(); 
+            services.AddTransient<IEmailSender, EmailSender>(); // enables email sends
+            services.AddTransient<IBusinessPlanWriteOnlyRepository, BusinessPlanWriteOnlyRepository>(); 
+            return services;
+        }
+    }
+}
--- SuccessfulStartup.Presentation/Program.cs
+++ SuccessfulStartup.Presentation/Program.cs
@@ -1,29 +1,14 @@
-using Microsoft.AspNetCore.Components.Authorization; // necessary for AuthenticationStateProvider
-using Microsoft.AspNetCore.Identity.UI.Services; // necessary for IEmailSender
-using Microsoft.EntityFrameworkCore; // necessary for UseSqlServer
 using SuccessfulStartup.Data;
-using SuccessfulStartup.Data.APIs;
-using SuccessfulStartup.Data.Authentication; // assembly reference in order to access Identity database
-using SuccessfulStartup.Data.Contexts;
-using SuccessfulStartup.Data.Repositories.WriteOnly;
-using SuccessfulStartup.Domain.Repositories.WriteOnly;
 
 var builder = WebApplication.CreateBuilder(args); // initializes a builder for configuring a new web application
-
-var connectionString = builder.Configuration.GetConnectionString("IdentityConnectionString"); // gets database connection string from appsettings.json TODO: //move to data layer
 
 // configure service collection
 
 builder.Services.AddRazorPages(); // allows Razor components, routing, model binding, caching, and view engines
 builder.Services.AddServerSideBlazor(); // allows Blazor Server specific functions
-builder.Services.AddDbContextFactory<AuthenticationDbContext>(options => options.UseSqlServer(connectionString)); // ,ServiceLifetime.Scoped); TODO: move to Data layer
-builder.Services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<AuthenticationDbContext>(); // adds default UI for Identity, eliminating need to create custom register and login pages, also requires account verification prior to first login TODO: move to data layer
-builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<AppUser>>(); // periodically checks whether user credentials are still valid //TODO: move to Data Layer
-builder.Services.AddScoped<IWriteOnlyApi, WriteOnlyApi>(); // TODO: move to data layer
-builder.Services.AddScoped<IReadOnlyApi, ReadOnlyApi>(); // TODO: move to data layer
-builder.Services.AddScoped<MappingProfile>();
-builder.Services.AddTransient<IEmailSender, EmailSender>(); // enables email sends // TODO: move to data layer
-builder.Services.AddTransient<IBusinessPlanWriteOnlyRepository, BusinessPlanWriteOnlyRepository>(); // TODO: move to data layer
+
+DataLayerConfiguration.AddDataScope(builder.Services); // adds services defined in the data project
+
 
 var app = builder.Build(); // initializes web application from builder
 
