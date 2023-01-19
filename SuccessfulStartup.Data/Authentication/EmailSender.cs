af345c2896ca79b0ad364d70eddc47b2c883d271
Tim Honchel <tim.honchel@gmail.com>
2023-01-19 15:30:40 -0500

    wrote and passed tests for EmailSender

--- SuccessfulStartup.Data/Authentication/EmailSender.cs
+++ SuccessfulStartup.Data/Authentication/EmailSender.cs
@@ -2,34 +2,50 @@
 using Microsoft.Extensions.Configuration; // for accessing connection strings
 using System.Net; // for creating NetworkCredential
 using System.Net.Mail; // for composing email with MailMessage, MailAddress, and SmtpClient
+using System.Runtime.CompilerServices; // for InternalsVisibleto
+
+[assembly: InternalsVisibleTo("SuccessfulStartup.DataTests")] // allows tests to access internal members
 
 namespace SuccessfulStartup.Data.Authentication
 {
     internal class EmailSender : IEmailSender // interface is for sending Identity-related emails
     {
+        private static IConfigurationRoot _configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build(); // gets the info from appsettings
+        internal static string _fromMail = _configuration["ConnectionStrings:GmailAddress"]; // gets Gmail account used for sending emails
+        internal static string _fromPassword = _configuration["ConnectionStrings:GmailPassword"]; // gets app password
+
         public async Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build(); // gets the info from appsettings
 
-            string fromMail = configuration["ConnectionStrings:GmailAddress"]; // gets Gmail account used for sending emails
-            string fromPassword = configuration["ConnectionStrings:GmailPassword"]; // gets app password
+            var message = CreateMessage(email, subject, htmlMessage);
+
+            var smtpClient = CreateSmtpClient();
+
+            smtpClient.Send(message);
+        }
+
+        public MailMessage CreateMessage(string email, string subject, string htmlMessage) // created separate class for single responsiblity principle and unit testing
+        {
+            if (String.IsNullOrWhiteSpace(email) || String.IsNullOrWhiteSpace(subject) || String.IsNullOrWhiteSpace(htmlMessage)) { throw new ArgumentNullException(); }
 
             var message = new MailMessage();
-            message.From = new MailAddress(fromMail);
+            message.From = new MailAddress(_fromMail);
             message.Subject = subject;
             message.To.Add(new MailAddress(email));
             message.Body = "<html><body>" + htmlMessage + "</body></html>";
             message.IsBodyHtml = true;
+            return message;
+        }
 
+        public SmtpClient CreateSmtpClient() // created separate class for single responsiblity principle and unit testing
+        {
             var smtpClient = new SmtpClient("smtp.gmail.com")
             {
                 Port = 587,
-                Credentials = new NetworkCredential(fromMail, fromPassword),
+                Credentials = new NetworkCredential(_fromMail, _fromPassword),
                 EnableSsl = true,
-
             };
-
-            smtpClient.Send(message);
+            return smtpClient;
         }
     }
 }
--- SuccessfulStartup.DataTests/Authentication/EmailSenderTests.cs
+++ SuccessfulStartup.DataTests/Authentication/EmailSenderTests.cs
@@ -1,6 +1,70 @@
-﻿namespace SuccessfulStartup.DataTests.Authentication
+﻿using Shouldly; // for assertion
+using SuccessfulStartup.Data.Authentication;
+using System.Net.Mail; // for MailMessage, MailAddress, MailAddressCollection
+
+namespace SuccessfulStartup.DataTests.Authentication
 {
+    [TestFixture]
     public class EmailSenderTests
     {
+        private EmailSender _sender;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _sender = new EmailSender();
+        }
+
+        [Test]
+        public void GetsFromEmailFromAppSettings()
+        {
+            EmailSender._fromMail.ShouldNotBeNullOrWhiteSpace();
+        }
+
+        [Test]
+        public void GetsEmailPasswordFromAppSettings()
+        {
+            EmailSender._fromPassword.ShouldNotBeNullOrWhiteSpace();
+        }
+
+        [Test]
+        public void CreateMessage_ReturnsMailMessage_GivenValidInput()
+        {
+            var message = _sender.CreateMessage("email@gmail.com", "subject", "htmlMessage");
+            message.ShouldBeOfType<MailMessage>();
+        }
+
+        [Test]
+        public void CreateMessage_ReturnsValidMailMessage_GivenValidInput()
+        {
+            var message = _sender.CreateMessage("email@gmail.com", "subject", "htmlMessage");
+
+            message.ShouldSatisfyAllConditions(
+                () => message.From.ShouldBeEquivalentTo(new MailAddress(EmailSender._fromMail)),
+                () => message.Subject.ShouldBe("subject"),
+                () => message.To.ShouldBeEquivalentTo(new MailAddressCollection { new MailAddress("email@gmail.com") }), // To is a MailAddressCollection because it could include multiple addresses
+                () => message.Body.ShouldBe("<html><body>htmlMessage</body></html>")
+                ); 
+        }
+
+        [TestCase("", "subject", "htmlMessage")]
+        [TestCase("email@gmail.com", null, "htmlMessage")]
+        [TestCase("email@gmail.com", "subject", "    ")]
+        public void CreateMessage_ThrowsArgumentNullException_GivenNullOrEmptyInput(string email, string subject, string htmlMessage)
+        {
+         Should.Throw<ArgumentNullException>( () => _sender.CreateMessage(email, subject, htmlMessage));       
+        }
+
+        [Test]
+        public void CreateMessage_ThrowsInvalidFormatException_GivenInvalidEmail()
+        {
+            Should.Throw<FormatException>(() => _sender.CreateMessage("emailWithoutRequiredCharacters", "subject", "htmlMessage"));
+        }
+
+        [Test]
+        public void CreateSmtpClient_ReturnsSmtpClient()
+        {
+            Assert.Ignore();
+        }
     }
 }
